10bdeaa9d3d3857daf58413879c88ff5e7e014ce
Markus <Markus-Clephas@t-online.de>
2025-01-22 11:58:49 +0100

    1./ Vererbung von Parent zu Child und von Grandparent zu Parent/Child

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,13 +4,16 @@ public class App {
     
     public static void main(String[] args) {
      
+        Child child = new Child();
+        output(child.sayHello());
+        output(child.sayHello("Hi from "));
+        
        
-       
-
     }
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
+
     }
 
 
---
+++ com/cc/java/ParentSuper.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class ParentSuper extends GrandParent {
+
+    // public String sayHello(){
+    //     return "Hello from parent!";
+    // }
+    
+    public String sayHello(String str){
+        return str + "parent!";
+    }
+
+}
